72fce0189fde9f1955cc988a08084466d88628e6
Christine Bumpous <bumpouce@gmail.com>
2020-01-22 19:15:43 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -7,21 +7,14 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles 
-  row_index = 0 
-  nds = directors_database
+  nds = directors_database[0][:movies]  #movies from 1st director
   movie_titles = []
+  movie_index = 0
   
-  while row_index < nds.length do
-    if (nds[row_index][:name] == "Stephen Spielberg") 
-      movie_index = 0
-      collection = nds[row_index][:movies]
-
-      while movie_index < collection.length do
-        movie_titles << collection[movie_index][:title]
-        movie_index += 1
-      end
-    end
-    row_index += 1
+  while movie_index < nds.length do
+    movie_titles << nds[movie_index][:title]
+    movie_index += 1
   end
+
   puts movie_titles.join ("\n")
 end
